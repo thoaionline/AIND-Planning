d47c17199afbf266615687769d6d64bfadcc7b8b
Thoai Nguyen <me@thoaionline.com>
2017-04-14 21:19:06 +1000

    Load possible actions

--- my_air_cargo_problems.py
+++ my_air_cargo_problems.py
@@ -46,7 +46,6 @@ class AirCargoProblem(Problem):
             list of Action objects
         '''
 
-        # TODO create concrete Action objects based on the domain action schema for: Load, Unload, and Fly
         # concrete actions definition: specific literal action that does not include variables as with the schema
         # for example, the action schema 'Load(c, p, a)' can represent the concrete actions 'Load(C1, P1, SFO)'
         # or 'Load(C2, P2, JFK)'.  The actions for the planning problem must be concrete because the problems in
@@ -74,7 +73,7 @@ class AirCargoProblem(Problem):
                         ]
 
                         load = Action(
-                            expr("Load({}, {}, {}".format(cargo, plane, airport)),
+                            expr("Load({}, {}, {})".format(cargo, plane, airport)),
                             [precond_pos, precond_neg],
                             [effect_add, effect_rem]
                         )
@@ -145,8 +144,23 @@ class AirCargoProblem(Problem):
             e.g. 'FTTTFF'
         :return: list of Action objects
         """
-        # TODO implement
         possible_actions = []
+
+        state = decode_state(state,self.state_map)
+
+        for action in self.actions_list:
+            ok = True
+            for precond_pos in action.precond_pos:
+                if not (precond_pos in state.pos):
+                    ok = False
+                    break
+            for precond_nev in action.precond_neg:
+                if not (precond_nev in state.neg):
+                    ok = False
+                    break
+            if ok:
+                possible_actions.append(action)
+
         return possible_actions
 
     def result(self, state: str, action: Action):
